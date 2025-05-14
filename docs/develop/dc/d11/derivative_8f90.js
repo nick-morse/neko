2cce3839373c4eca75e90b7013ddfbdcfc87c2a2
njansson <njansson@users.noreply.github.com>
2025-05-14 03:47:26 +0000

    deploy: e3674c54ba221ce866093c357b4038cf79778698

--- docs/develop/dc/d11/derivative_8f90.js
+++ docs/develop/dc/d11/derivative_8f90.js
@@ -3,6 +3,8 @@ var derivative_8f90 =
     [ "derivative::derivative_t", "db/d12/structderivative_1_1derivative__t.html", "db/d12/structderivative_1_1derivative__t" ],
     [ "derivative_compute", "dc/d11/derivative_8f90.html#a21a4d8c65632735bfec3220228ac4f0c", null ],
     [ "derivative_free", "dc/d11/derivative_8f90.html#a39b775f5a1e401f7e18ad700c23654ba", null ],
-    [ "derivative_init_from_components", "dc/d11/derivative_8f90.html#a81321ce8f44678a803647314e883a6c4", null ],
+    [ "derivative_init_common", "dc/d11/derivative_8f90.html#a4cdee76c9e81a0ab7cca55574f55efeb", null ],
+    [ "derivative_init_from_controllers", "dc/d11/derivative_8f90.html#a4c1b414a2e1ee8b8e3087c641141632f", null ],
+    [ "derivative_init_from_controllers_properties", "dc/d11/derivative_8f90.html#aff00ccc100e8b8138440ccddfbc0a00e", null ],
     [ "derivative_init_from_json", "dc/d11/derivative_8f90.html#acd82a2cfad3733928800db9f3bf33a9d", null ]
 ];
